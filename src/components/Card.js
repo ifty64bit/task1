0baa726fd6834e046bd6f6922be3635c9ae1ae81
ifty64bit <ifty64bit@gmail.com>
2022-02-15 11:57:11 +0600

    final

--- src/components/Card.js
+++ src/components/Card.js
@@ -15,7 +15,16 @@ function Card({data}) {
         <div id="about" className='pt-14 px-4'>
           <h2 className='text-xl'>{ data.meta.companyName}</h2>
           <div className='flex gap-x-2'><div className='text-gray-600'>{`${data.addresses[0].city} ${data.addresses[0].country}`}</div>.<div className='text-green'>Min Qty: { data.minOrderQty }</div></div>
-          <div id='tag' className='overflow-hidden text-gray-600'>{data.meta.companyShortDesc}</div>
+          <div id='tag' className='overflow-hidden text-gray-600'>
+            <ul className='flex gap-x-1'>
+              {data.productGroups.map(pg => {
+                return (
+                  <li key={pg.uid}>{ `${pg.name},` }</li>
+                )
+              })}
+            </ul>
+            
+          </div>
           <div id='button'>
             <button className='bg-green-light text-green-dark my-5 px-2 py-1 rounded-full min-w-[8rem]'>View Details</button>
           </div>
